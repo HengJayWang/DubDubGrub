7d0c0d8e913cbac945c02c9587612aa727d9e052
HengJay <hengjay.wang@itri.org.tw>
2022-02-08 16:09:13 +0800

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by M100-M1MacMini on 2022/2/8.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
