6e45db7e72407f855fcda1589ef4b8d32eeb6ac3
HengJay <hengjay.wang@itri.org.tw>
2022-02-08 17:31:46 +0800

    LocationMapView & LocationListView prototype.

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -9,12 +9,54 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("Location List View")
+        NavigationView {
+            List {
+                ForEach(0..<9) { item in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Test Location Name")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            
+                            HStack {
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                            }
+                        }
+                        .padding(.leading)
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+            .listStyle(.plain)
+        }
     }
 }
 
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
     }
 }
--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                  longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                          longitudeDelta: 0.01))
+    
     var body: some View {
-        Text("Map View")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+                
+                Spacer()
+            }
+        }
     }
 }
 
