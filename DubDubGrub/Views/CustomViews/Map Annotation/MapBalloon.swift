7494ff8524c67e0194b6e2d99333fbdda8d25988
HengJay <hengjay.wang@itri.org.tw>
2022-02-22 17:19:14 +0800

    Custom Map Annotation - Shape

---
+++ DubDubGrub/Views/CustomViews/Map Annotation/MapBalloon.swift
@@ -0,0 +1,30 @@
+//
+//  MapBalloon.swift
+//  DubDubGrub
+//
+//  Created by M100-M1MacMini on 2022/2/22.
+//
+
+import SwiftUI
+
+struct MapBalloon: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.minY),
+                          control: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.maxY),
+                          control: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        return path
+    }
+
+}
+
+struct MapBalloon_Previews: PreviewProvider {
+    static var previews: some View {
+        MapBalloon()
+            .frame(width: 300, height: 240)
+            .foregroundColor(.brandPrimary)
+    }
+}
